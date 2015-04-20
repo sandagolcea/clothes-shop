4d17c9105c412d047781b471f94f101a00fc43d9
Sanda Golcea <sanda.golcea@gmail.com>
2015-04-20 13:06:27 +0100

    Cart styling added

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@ var app = angular.module('clothesShop', []);
 
 app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
   var shop = this;
-  var cart = new ShoppingCart();
-  shop.itemsInCart = cart.items;
+  $scope.cart = new ShoppingCart();
 
   $http.get('products.json').success(function(data){
     shop.products = data;
@@ -12,7 +11,7 @@ app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
   $scope.addToCart = function (product) {
     if (product.quantity > 0) {
       product.quantity -= 1;
-      cart.addItem(product.pid, product.name, product.price, 1);
+      $scope.cart.addItem(product.pid, product.name, product.price, 1);
     }
   }
 }]);
--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,21 +1,28 @@
 function ShoppingCart () {
-  cart = this;
-  cart.items = [];
+  this.items = [];
 }
 
 ShoppingCart.prototype.addItem = function(pid, name, price, quantity) {
-  if ( item = cart._contains(pid) )
+  if ( item = this._contains(pid) )
     item.quantity += 1;
   else
-    cart.items.push({pid: pid, name: name, price: price, quantity: quantity});
+    this.items.push({pid: pid, name: name, price: price, quantity: quantity});
 };
 
 ShoppingCart.prototype._contains = function(pid) {
   var i;
-  for (i = 0; i < cart.items.length; i++) {
-    if ( cart.items[i].pid === pid ) {
-      return cart.items[i];
+  for (i = 0; i < this.items.length; i++) {
+    if ( this.items[i].pid === pid ) {
+      return this.items[i];
     }
   }
   return false;
+};
+
+ShoppingCart.prototype.totalItems = function() {
+  var total = 0;
+  var i;
+  for ( i = 0; i < this.items.length; i++)
+    total += this.items[i].quantity
+  return total;
 };
