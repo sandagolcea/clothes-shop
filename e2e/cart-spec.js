0a358914998802aecddcad532b625a8de90269f1
Sanda Golcea <sanda.golcea@gmail.com>
2015-05-05 22:12:20 +0100

    Adds some cart and shop tests

---
+++ e2e/cart-spec.js
@@ -0,0 +1,36 @@
+describe('angularjs shopping cart', function() {
+  var prodList = [];
+  var cartList = [];
+
+  beforeEach(function(){
+    browser.get('http://localhost:3000');
+    // prodList = element.all(by.repeater('product in products'));
+  });
+  
+  // TODO: DRY and separate to 2 test cases: 
+  // initial cond & item in cart
+  it('should have no item in cart to begin with', function () {
+    element(by.linkText('Your Cart')).
+    click();
+    cartList = element.all(by.repeater('item in cart.items'));
+    expect(cartList.count()).toEqual(0);
+  });
+
+  it('should add an item to cart when clicking on add to cart', function() {
+    // add product to cart
+    element.all(by.repeater('product in products'))
+    .get(1)
+    .element(by.buttonText('Add to cart'))
+    .click();
+    // goto cart
+    element(by.linkText('Your Cart')).
+    click();
+    // check one item in cart
+    cartList = element.all(by.repeater('item in cart.items'));
+    expect(cartList.count()).toEqual(1);
+  });
+
+  xit('should remove items from cart ',function () {
+    // remove item from cart
+  });
+});
--- e2e/conf.js
+++ e2e/conf.js
@@ -1,4 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['shop-spec.js']
+  specs: ['shop-spec.js','cart-spec.js']
 };
