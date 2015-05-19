612639206d11ed9f714b7066ae6e4df5d6af91d9
Sanda Golcea <sanda.golcea@gmail.com>
2015-05-19 23:44:16 +0100

    Tests: e2e tests for adding products and vouchers to cart, raw: needs refactoring

--- e2e/cart-spec.js
+++ e2e/cart-spec.js
@@ -1,16 +1,14 @@
-describe('angularjs shopping cart', function() {
+describe('Shopping cart', function() {
   var prodList = [];
   var cartList = [];
 
   beforeEach(function(){
     browser.get('http://localhost:3000');
   });
-  
-  // TODO: DRY and separate to 2 test cases: 
-  // initial cond & item in cart
+
   it('should have no item in cart to begin with', function () {
-    element(by.linkText('Your Cart')).
-    click();
+    element(by.linkText('Your Cart'))
+    .click();
     cartList = element.all(by.repeater('item in items'));
     expect(cartList.count()).toEqual(0);
   });
@@ -22,27 +20,102 @@ describe('angularjs shopping cart', function() {
     .element(by.buttonText('Add to cart'))
     .click();
     // goto cart
-    element(by.linkText('Your Cart')).
-    click();
+    element(by.linkText('Your Cart'))
+    .click();
     // check one item in cart
     cartList = element.all(by.repeater('item in items'));
     expect(cartList.count()).toEqual(1);
   });
 
+  it('should not add items Out of Stock', function() {
+    // add product to cart
+    element.all(by.repeater('product in products'))
+    .get(8)
+    .element(by.buttonText('Add to cart'))
+    .click();
+    // goto cart
+    element(by.linkText('Your Cart'))
+    .click();
+    // check one item in cart
+    cartList = element.all(by.repeater('item in items'));
+    expect(cartList.count()).toEqual(0);
+  });
+
   it('should remove items from cart ',function () {
-    // remove item from cart
+    // add item to cart
     element.all(by.repeater('product in products'))
     .get(1)
     .element(by.buttonText('Add to cart'))
     .click();
     // goto cart
-    element(by.linkText('Your Cart')).
-    click();
+    element(by.linkText('Your Cart'))
+    .click();
     // remove item
-    element(by.buttonText('Remove from cart')).
-    click();
+    element(by.buttonText('Remove from cart'))
+    .click();
     // check zero items in cart
     cartList = element.all(by.repeater('item in items'));
     expect(cartList.count()).toEqual(0);
   });
+
+  it('should display total price correctly', function () {
+    // add elem to cart
+    element.all(by.repeater('product in products'))
+    .get(1)
+    .element(by.buttonText('Add to cart'))
+    .click();
+    // goto cart
+    element(by.linkText('Your Cart'))
+    .click();
+    expect(element(by.id('totalPrice')).getText()).toBe('Total: £30.00');
+  });
+
+  it('should be able to have a voucher applied to the total', function () {
+    // add elem to cart
+    element.all(by.repeater('product in products'))
+    .get(1)
+    .element(by.buttonText('Add to cart'))
+    .click();
+    // goto cart
+    element(by.linkText('Your Cart'))
+    .click();
+    // apply voucher
+    element(by.model('voucherCode')).sendKeys('SHOP5OFF');
+    element(by.buttonText('Apply Voucher'))
+    .click();
+    expect(element(by.id('totalPrice')).getText()).toBe('Total: £25.00');
+  });
+
+  it('should confirm that a voucher has been applied', function () {
+    // add elem to cart
+    element.all(by.repeater('product in products'))
+    .get(1)
+    .element(by.buttonText('Add to cart'))
+    .click();
+    // goto cart
+    element(by.linkText('Your Cart'))
+    .click();
+    // apply voucher
+    element(by.model('voucherCode')).sendKeys('SHOP5OFF');
+    element(by.buttonText('Apply Voucher'))
+    .click();
+    expect(element(by.id('voucherMessage')).getText()).toBe('Voucher applied.');
+  });
+
+  it('should warn if a voucher can not be applied', function () {
+    // add elem to cart
+    element.all(by.repeater('product in products'))
+    .get(1)
+    .element(by.buttonText('Add to cart'))
+    .click();
+    // goto cart
+    element(by.linkText('Your Cart'))
+    .click();
+    // apply voucher
+    element(by.model('voucherCode')).sendKeys('EXPIRED');
+    element(by.buttonText('Apply Voucher'))
+    .click();
+    expect(element(by.id('voucherMessage')).getText()).toBe('Voucher not found or expired.');
+  });
+
 });
